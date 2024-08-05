a607b475d68fbad6ff8f48a1bf27186e62f18438
Cngo2019 <canngo215@gmail.com>
2024-08-05 11:37:19 -0400

    set up db connection. delete unneccssary folder

--- app.py
+++ app.py
@@ -5,14 +5,19 @@ from dotenv import load_dotenv
 import os
 app = Flask(__name__)
 
-
+load_dotenv()
 app.config['MYSQL_HOST'] = os.getenv('HOST')
 app.config['MYSQL_USER'] = os.getenv('USER')
 app.config['MYSQL_PASSWORD'] = os.getenv('PASSWORD')
-app.config['MYSQL_DB'] = os.get('DB')
+app.config['MYSQL_DB'] = os.getenv('DB')
 mysql = MySQL(app)
 
 
 @app.route("/")
 def hello_world():
+    curr = mysql.connection.cursor()
+    curr.execute('SELECT * FROM banking_system.bank_user')
+    rv = curr.fetchall()
+
+    print(rv)
     return "<p>Hello, World!</p>"
